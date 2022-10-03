89ca6c4f97d09c28171b0f1d1bd09adbbbdf82e9
Cauã de Andrade <104733922+caua075@users.noreply.github.com>
2022-10-03 11:14:03 -0300

    Add files via upload

---
+++ ListaExercicio01/Ex01_Lista01_Caua/main.cpp
@@ -0,0 +1,7 @@
+#include "metEx01.h"
+
+using namespace std;
+int main() {
+    metEx01 obj;
+    obj.lerPonto();
+}
---
+++ ListaExercicio01/Ex02_Lista01_Caua/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "metEx02.h"
+
+using namespace std;
+
+int main() {
+    metEx02 obj;
+    obj.lerNumeros();
+}
